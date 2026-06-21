d8334dad59df3d1c24863ef8022843af209b7a0c
AKSHIT <akshitpaliwal18@gmail.com>
2026-06-21 17:34:43 +0530

    connecting open-ai

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -8,8 +8,9 @@ import { prisma } from "./db";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.text({ type: ["application/sdp", "text/plain" ]}));
 
-app.post("/api/v1/pre-interview", async (req, res) => {
+    app.post("/api/v1/pre-interview", async (req, res) => {
     
         const { success, data } = PreInterviewBody.safeParse(req.body);
         
@@ -40,13 +41,39 @@ app.post("/api/v1/pre-interview", async (req, res) => {
 
             }
         })
-
         res.json({ id: interview.id });
-    }
-} catch(e){
-    console.log(e)
-}
-)
+        } catch(e){
+            console.log(e)
+        }
+    })
 
+    app.post("api/v1/session", async (req, res) => {
+            const sessionConfig = JSON.stringify({
+                type: "realtime",
+                model: "gpt-realtime", 
+                audio: { output: {voice: "cedar" }},
+            })
+
+            const fd = new FormData();
+            fd.set("sdp", req.body);
+            fd.set("session", sessionConfig);
+
+            try {
+                const r = await fetch("https://api.openai.com/v1/realtime/calls", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                    "OpenAI-Safety-Identifier": "hashed-user-id",
+                },
+                body: fd,
+                });
+                // Send back the SDP we received from the OpenAI REST API
+                const sdp = await r.text();
+                res.send(sdp);
+            } catch (error) {
+                console.error("Token generation error:", error);
+                res.status(500).json({ error: "Failed to generate token" });
+            }
+} )
 
 app.listen(3001);
